7007ea1d5909fd71d9c42c51bb80edf15bd82d24
Chris Shaw <shaw64489@yahoo.com>
2019-06-18 06:20:42 -0400

    routing

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,8 @@ import Router from 'vue-router';
 
 import PageHome from '@/pages/PageHome';
 import PageMeetupDetail from '@/pages/PageMeetupDetail';
+import PageMeetupFind from '@/pages/PageMeetupFind';
+import PageNotFound from '@/pages/PageNotFound';
 
 
 Vue.use(Router)
@@ -15,10 +17,21 @@ const router = new Router({
             component: PageHome
         },
         {
+            path: '/find',
+            name: 'PageMeetupFind',
+            component: PageMeetupFind
+        },
+        {
             path: '/meetups/:id',
             name: 'PageMeetupDetail',
             component: PageMeetupDetail
+        },
+        {
+            path: '*',
+            name: 'PageNotFound',
+            component: PageNotFound
         }
+
     ],
     mode: 'history'
 })
